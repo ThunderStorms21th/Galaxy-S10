0ef18542f75d2b64d6c5cc91e5c1efcd98a424c5
Wei Wang <wvw@google.com>
2018-02-28 13:34:22 -0800

    mm: add config for default readahead size
    Change the VM_MAX_READAHEAD value from the default 64KB to a
    configurable value. This will allow the readahead window to grow to a
    maximum size bigger than 128KB, which greatly benefits to sequential
    read throughput and thus boot performance.
    
    Change-Id: Iad448cf1198056de46654dcb409466802b3b908d
    Signed-off-by: Wei Wang <wvw@google.com>

--- include/linux/mm.h
+++ include/linux/mm.h
@@ -2246,7 +2246,7 @@ int __must_check write_one_page(struct page *page);
 void task_dirty_inc(struct task_struct *tsk);
 
 /* readahead.c */
-#define VM_MAX_READAHEAD	64	/* 128 kbytes */
+#define VM_MAX_READAHEAD	CONFIG_VM_MAX_READAHEAD	/* kbytes - default 128 kbytes */
 #define VM_MIN_READAHEAD	16	/* 16 kbytes (includes current page) */
 extern int mmap_readaround_limit;
 
