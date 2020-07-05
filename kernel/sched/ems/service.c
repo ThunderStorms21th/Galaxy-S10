a3198fcb560f7b35ab97808dabd7cd4ab51f065f
Diep Quynh <remilia.1505@gmail.com>
2020-07-05 23:58:19 +0700

    sched: ems: service: Fix service CPU selection algorithm
    1. Consider boosted task utilization when choosing/considering
    maximum spare capacity
    2. Take advantage of CPU cstates
    3. Account of task utilization to avoid overutilizing a CPU
    
    Signed-off-by: Diep Quynh <remilia.1505@gmail.com>

--- kernel/sched/ems/service.c
+++ kernel/sched/ems/service.c
@@ -116,6 +116,7 @@ select_prefer_cpu(struct task_struct *p, int coregroup_count, struct cpumask *pr
 	struct cpumask mask;
 	int coregroup, cpu;
 	unsigned long max_spare_cap = 0;
+	unsigned long min_idle_util = ULONG_MAX;
 	int best_perf_cstate = INT_MAX;
 	int best_perf_cpu = -1;
 	int backup_cpu = -1;
@@ -131,18 +132,27 @@ select_prefer_cpu(struct task_struct *p, int coregroup_count, struct cpumask *pr
 			unsigned long capacity_orig;
 			unsigned long wake_util_with;
 			unsigned long wake_util_without;
+			unsigned long capacity_orig = capacity_orig_of(cpu);
+			unsigned long new_util = cpu_util_wake(cpu, p) + task_util_est(p);
 
 			if (cpu >= nr_cpu_ids)
 				break;
+
+			new_util = max(new_util, boosted_task_util(p));
+			if (new_util > capacity_orig)
+				continue;
 
 			if (idle_cpu(cpu)) {
 				int idle_idx = idle_get_state_idx(cpu_rq(cpu));
 
 				/* find shallowest idle state cpu */
-				if (idle_idx >= best_perf_cstate)
+				if (idle_idx > best_perf_cstate ||
+				   (idle_idx == best_perf_cstate &&
+					new_util >= min_idle_util))
 					continue;
 
 				/* Keep track of best idle CPU */
+				min_idle_util = new_util;
 				best_perf_cstate = idle_idx;
 				best_perf_cpu = cpu;
 				continue;
@@ -160,6 +170,12 @@ select_prefer_cpu(struct task_struct *p, int coregroup_count, struct cpumask *pr
 				continue;
 
 			max_spare_cap = capacity_orig - wake_util_without;
+
+			if ((capacity_orig - new_util) < max_spare_cap)
+				continue;
+
+			max_spare_cap = capacity_orig - new_util;
+
 			backup_cpu = cpu;
 		}
 
