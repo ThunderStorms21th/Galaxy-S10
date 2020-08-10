14b956c41f5b909ee2306ac86036456574b69839
ThunderStorms21th nalas <pinakastorm@gmail.com>
2020-08-10 19:16:02 +0200

    rearanged ts-build.sh
    - added start and end time compile build
    - added builds folder for flashable img file with specific name
    - added auto rename img file with compile date
    
    Signed-off-by: ThunderStorms21th nalas <pinakastorm@gmail.com>

--- ts-build.sh
+++ ts-build.sh
@@ -3,4 +3,31 @@
 # Kernel Build Script v1.0 by ThunderStorms Team
 #
 
-./build mkimg model=G973F name="ThunderStorms-S10-v1.0"
+LOG=compile_build.log
+
+# MAIN PROGRAM
+# ------------
+
+MAIN()
+{
+
+(
+	START_TIME=`date +%T`
+    ./build mkimg model=G973F name="ThunderStorms-S10-v1.0"
+	END_TIME=`date +%T`
+	echo "Start compile time is $START_TIME"
+	echo "Start compile time is $END_TIME"
+	echo ""
+	echo "Your flasheable release can be found in the builds folder with name :"
+	echo "ThunderStorms-S10-v1.0-`date +%Y-%m-%d`.img"
+	echo ""
+) 2>&1 | tee -a ./$LOG
+}
+
+# RUN PROGRAM
+# -----------
+
+MAIN
+
+cp -f boot.img builds/ThunderStorms-S10-v1.0-`date +%Y-%m-%d`.img
+
