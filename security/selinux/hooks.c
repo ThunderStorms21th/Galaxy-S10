016408a6b56c43a89f5fcc77392aaf0164c072c5
ThunderStorms21th <pinakastorm@gmail.com>
2020-11-30 14:26:15 +0100

    FROMLIST: security: selinux: allow per-file labelling for binderfs
    org commit: https://github.com/UtsavBalar1231/kernel_xiaomi_raphael/commit/5bbf2879a5bddbf4e8f8a18cabaae9ea5d6ec3b8
    
    This patch allows genfscon per-file labeling for binderfs.
    This is required to have separate permissions to allow
    access to binder, hwbinder and vndbinder devices which are
    relocating to binderfs.
    
    Acked-by: Jeff Vander Stoep <jeffv@google.com>
    Acked-by: Mark Salyzyn <salyzyn@android.com>
    Signed-off-by: Hridya Valsaraju <hridya@google.com>
    
    Bug: 136497735
    (cherry picked from commit 7a4b51947475a7f67e2bd06c4a4c768e2e64a975
    git://git.kernel.org/pub/scm/linux/kernel/git/pcmoore/selinux.git
    master)
    Link: https://lore.kernel.org/patchwork/patch/1175776/
    Change-Id: I105cc54b30ddd4120dc23a363bddc2f9d00e4dc4
    
    Signed-off-by: ThunderStorms21th <pinakastorm@gmail.com>

--- security/selinux/hooks.c
+++ security/selinux/hooks.c
@@ -603,6 +603,7 @@ static int selinux_is_genfs_special_handling(struct super_block *sb)
 	/* Special handling. Genfs but also in-core setxattr handler */
 	return	!strcmp(sb->s_type->name, "sysfs") ||
 		!strcmp(sb->s_type->name, "pstore") ||
+		!strcmp(sb->s_type->name, "binder") ||
 		!strcmp(sb->s_type->name, "debugfs") ||
 		!strcmp(sb->s_type->name, "tracefs") ||
 		!strcmp(sb->s_type->name, "rootfs") ||
