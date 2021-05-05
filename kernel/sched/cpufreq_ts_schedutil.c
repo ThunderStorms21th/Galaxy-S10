6435288127274ecc0c3c10f99d56d0f539a625f6
ThunderStorms21th <pinakastorm@gmail.com>
2021-05-05 11:27:05 +0200

    schedutil_ts: restore cached freq when next_f is not changed
    We have the raw cached freq to reduce the chance in calling cpufreq
    driver where it could be costly in some arch/SoC.
    
    Currently, the raw cached freq is reset in sugov_update_single() when
    it avoids frequency reduction (which is not desirable sometimes), but
    it is better to restore the previous value of it in that case,
    because it may not change in the next cycle and it is not necessary
    to change the CPU frequency then.
    
    Adapted from https://android-review.googlesource.com/1352810/
    
    Signed-off-by: Wei Wang <wvw@google.com>
    Acked-by: Viresh Kumar <viresh.kumar@linaro.org>
    [ rjw: Subject edit and changelog rewrite ]
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    
    org commit : https://github.com/Freescale/linux-fslc/commit/0070ea29623904224c0f5fa279a16a4ac9223295
    
    Signed-off-by: ThunderStorms21th <pinakastorm@gmail.com>

--- kernel/sched/cpufreq_ts_schedutil.c
+++ kernel/sched/cpufreq_ts_schedutil.c
@@ -600,6 +600,7 @@ static void sugov_update_shared(struct update_util_data *hook, u64 time,
 	struct sugov_policy *sg_policy = sg_cpu->sg_policy;
 	unsigned long util, max;
 	unsigned int next_f;
+	unsigned int cached_freq = sg_policy->cached_raw_freq;
 
 	sugov_get_util(&util, &max, sg_cpu->cpu);
 
@@ -623,6 +624,9 @@ static void sugov_update_shared(struct update_util_data *hook, u64 time,
 			next_f = sg_policy->policy->cpuinfo.max_freq;
 		else
 			next_f = sugov_next_freq_shared(sg_cpu, time);
+
+		/* Restore cached freq as next_freq has changed */
+		sg_policy->cached_raw_freq = cached_freq;
 
 	/*
 	 * This code runs under rq->lock for the target CPU, so it won't run
